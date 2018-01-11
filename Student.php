e86caceedcec44f2d6080f2f823bc03c9e5662d7
William Murphy <will.murphy1994@gmail.com>
2018-01-11 14:22:12 -0800

    Add comments

--- Student.php
+++ Student.php
@@ -6,6 +6,9 @@
  */
 class Student {
 
+    /**
+     * Contructor
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -13,14 +16,26 @@ class Student {
         $this->grades = array();
     }
 
+    /**
+     * Adds email for a student
+     * @param which Where the email goes in the email array
+     * @param address The email address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Add a grade to a student.
+     * @param grade The grade to add.
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /**
+     * Calculates the average grade of a student.
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value) {
@@ -29,9 +44,13 @@ class Student {
         return $total / count($this->grades);
     }
 
+    /**
+     * Converts a student object into its string representation.
+     * @return String The student object formatted into a string.
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' (' . $this->average().")\n"
+        $result .= ' (' . $this->average().")\n";
         foreach ($this->emails as $which => $what) {
             $result .= $which . ': ' . $what . "\n";
         }
